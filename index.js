8116b98450303368e22e7b30e6fc62c7d730cbd6
Abusolay <sefat01625@gmail.com>
2025-12-20 20:21:45 +0600

    sum function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //     return [...new Set(arr)]
 // }
 
-const { useEffect } = require("react")
+// const { useEffect } = require("react")
 
 // console.log(removeDuplicates([1,2,3,2,3,4,4,5,5]))
 
@@ -16,60 +16,84 @@ const { useEffect } = require("react")
 //    console.log(x);
 //    let x = 20;
 // })();
-console.log(foo)
-var foo ="hello "
-console.log(foo)
+// console.log(foo)
+// var foo ="hello "
+// console.log(foo)
 
-app.get("data", async (req,res) => {
-    const result = fetch ("httpy://api.exmple.com/data")
-    res.send(await result.json())
-})
+// app.get("data", async (req,res) => {
+//     const result = fetch ("httpy://api.exmple.com/data")
+//     res.send(await result.json())
+// })
 
-app.use((req, res, next)=> {
-    console.log("Request received")
-})
+// app.use((req, res, next)=> {
+//     console.log("Request received")
+// })
 
-function sum(...numbers, extra){
-    return numbers.reduce((acc, num) => acc +num, 0)+ extra
-}
-function MYComponent(){
-    const inputRef = useRef()
-    useEffect(() => {
-inputRef.current.focus()
-    }, [])
-    return <input/>
-}
+// function sum(...numbers, extra){
+//     return numbers.reduce((acc, num) => acc +num, 0)+ extra
+// }
+// function MYComponent(){
+//     const inputRef = useRef()
+//     useEffect(() => {
+// inputRef.current.focus()
+//     }, [])
+//     return <input/>
+// }
 
-<div onClick="console.log('div clicked')">
-<button onClick="console.log('buttonclicked')">click me</button>
+// <div onClick="console.log('div clicked')">
+// <button onClick="console.log('buttonclicked')">click me</button>
 
-</div>
+// </div>
 
-console.log(1 +"2" +3)
+// console.log(1 +"2" +3)
 
-function fetchData(){
-    let data;
-    fetch("httpy://api.exmple.com/data")
-    .then(response => response.json())
-    .then(json => data = json)
-    return data
-}
+// function fetchData(){
+//     let data;
+//     fetch("httpy://api.exmple.com/data")
+//     .then(response => response.json())
+//     .then(json => data = json)
+//     return data
+// }
 
-const a = [1,2,3]
-const b = a
-b.push(4)
-console.log(a)
+// const a = [1,2,3]
+// const b = a
+// b.push(4)
+// console.log(a)
 
-let x = 1
-let y = 2
-x = x+y++
-console.log(x,y)
+// let x = 1
+// let y = 2
+// x = x+y++
+// console.log(x,y)
 
-let x = [1,2,3]
-let y = [...x]
-y[0] = 4
-console.log(x[0] , y[0])
+// let x = [1,2,3]
+// let y = [...x]
+// y[0] = 4
+// console.log(x[0] , y[0])
 
-const numbers = [1, 2,3]
-numbers[10] = 10
-console.log(numbers.length)
+// const numbers = [1, 2,3]
+// numbers[10] = 10
+// console.log(numbers.length)
+
+// function outer() {
+//   let x = 10;
+//   return function inner() {
+//     console.log(x);
+//   };
+// }
+
+// const arr1 = [1, 2,3,4];
+// const arr2 = [3,4,5,6]
+// const intersection = arr1.filter(x => arr2.includes(x));
+// console.log(intersection)
+const a = 5 ;
+const b= 6 ;
+
+ function sum (a, b){
+  return a + b
+ }
+
+ function greet(name, callback){
+  console.log(" hello" + name)
+  callback()
+ }
+ console.log(sum(5,6))
